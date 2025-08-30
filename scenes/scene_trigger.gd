96ecd448398d166692a8fa5c034700ffe3b5f064
Luke Leinonen <lmleinon@mtu.edu>
2025-08-30 01:08:50 -0400

    Initial Changes/Fixes
    - Pixel art looked blurry, so I changed the rendering texture filter from Linear to Nearest. Now the pixel art is nice and crisp
    - Added a transitions physics layer
    - Streamlined a few things in the player.gd to simplify it a bit
    - Prototyped a fix with crouching
    - Made slope movement constant and sticks to the ground on a decline
    - I re-enabled the esc menu in the throne room so I could close the game
    - I changed the transition areas to only watch for layer 10 (transitions), which I set the player to also be in
    - Fixed an error in scene_trigger.gd where the incoming _body object couldn't be made into a different object, fixed with a specific transitions layer instead of the normal layer 1 deal
    - The exit button works on the title screen. The signal wasn't connected to the script
    - You were still able to click the pause menu buttons while they were invisible, so I disabled them when they are invisible (or more specifically, when they become invisible)
    - I renamed value to newValue in volume_slider.gd because it was shadowing an established variable in the Slider class
    
    If I misinterpreted any aspects, such as constant speed on slopes and sticking to slopes, please let me know.

--- scenes/scene_trigger.gd
+++ scenes/scene_trigger.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 @export var path: String
 
-func _on_body_entered(_body: PhysicsBody2D) -> void:
+func _on_body_entered(_body) -> void:
+	print(type_string(typeof(_body)))
 	get_tree().call_deferred("change_scene_to_file", path)
