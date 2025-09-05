6560d929b2402d1709567fd254617d76369dc5ee
Luke Leinonen <lmleinon@mtu.edu>
2025-09-05 01:21:03 -0400

    Collisions, Combat, and Health
    - I added more physics layers
        - environment
            - for environmental physics
        - player
            - for player detection
        - playerAttack
            - for detecting player attacks
        - enemy
            - for enemy detection
        - enemyAttack
            - for detecting enemy attacks (like projectiles)
    - Added functional health display
        - When taking damage, remove half a heart per unit of damage taken
        - Similar functionality for healing, though the actual healing isn't implemented
        - The current number of hearts is set to 3; dynamic maximum health will come later
    - Added functional hit recovery
        - When the player takes damage, they become invincible for immortalityDuration
            - Someone wrote immortality with 3 m's, but I fixed it
    - Renamed a bunch of things to improve clarity (understand their use by their name)

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,7 +15,7 @@ signal facing_changed(facing_right: bool)
 var facing_right = true
 
 func _ready(): 
-	connect("facing_changed", Callable($AnimatedSprite2D2, "_on_facing_changed"))
+	connect("facing_changed", Callable($AnimatedWeaponSprite, "_on_facing_changed"))
 
 func set_facing(facingAxis: float) -> void: 
 	if (facingAxis > 0):
@@ -38,7 +38,7 @@ func _physics_process(delta):
 	else:
 		velocity.x += walk * delta
 	velocity.x = clamp(velocity.x, -max_run_speed, max_run_speed) if Input.is_action_pressed("run") else clamp(velocity.x, -max_walk_speed, max_walk_speed)
-	$AnimatedSprite2D.scale = Vector2(0.2, 0.1) if Input.is_action_pressed("crouch_look_down") else Vector2(0.2, 0.2)
+	$AnimatedPlayerSprite.scale = Vector2(0.2, 0.1) if Input.is_action_pressed("crouch_look_down") else Vector2(0.2, 0.2)
 	
 	# This shrinks the player so they can go under shorter areas, whoever, it gets stuck on the floor
 	#$CollisionShape2D.scale = Vector2(1, 0.5) if Input.is_action_pressed("crouch_look_down") else Vector2(1, 1)
@@ -56,15 +56,18 @@ func _physics_process(delta):
 				stone.play()
 		else: 
 			stone.stop()
-		$AnimatedSprite2D.play()
+		$AnimatedPlayerSprite.play()
 	else: 
 		stone.stop()
-		$AnimatedSprite2D.stop()
+		$AnimatedPlayerSprite.stop()
 	
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedPlayerSprite.animation = "walk"
+		$AnimatedPlayerSprite.flip_v = false
+		$AnimatedPlayerSprite.flip_h = velocity.x < 0
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_speed
+
+func _on_health_health_depleted() -> void:
+	get_tree().quit()
--- scenes/scene_trigger.gd
+++ scenes/scene_trigger.gd
@@ -3,5 +3,4 @@ extends Area2D
 @export var path: String
 
 func _on_body_entered(_body) -> void:
-	print(type_string(typeof(_body)))
 	get_tree().call_deferred("change_scene_to_file", path)
