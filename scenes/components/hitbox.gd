784731c7b06f08ef64a9e769dee853d35ea054d4
LXHarris <lxharris@mtu.edu>
2025-09-16 06:37:27 -0400

    A Whole Lotta Changes
    I will be commenting as much as I can but it's 6:30 AM.
    
    Overhauled the hitbox system. Hitboxes monitor while Hurtboxes are monitorable. Easy setup and hopefully quite future proofed.
    
    Reworked player attack, created a new "Attack" component that is part of the aformentioned future proofing. For now it's just a vestigial wrapper but can hopefully be used for designating types of attacks and resolving them by itself.
    
    Added Coyote Time, possible infinite jumping bug though I think I fixed it. Looking to add variable jump height next
    
    Added intended movement variables and removed "facing" code as sprite flipping was handled by velocity already and the "Attack" wrapper allows for an easier method of flipping where the sword hitbox comes from. Ensured the player cannot rapidly flip hitboxes back and forth.

--- scenes/components/hitbox.gd
+++ scenes/components/hitbox.gd
@@ -1,19 +1,42 @@
 class_name HitBox
 extends Area2D 
 
-@export var damage: int = 1: set = set_damage, get = get_damage 
+@export var enabled: bool = true: set = set_enabled, get = get_enabled
+@export var damage: int = 1: set = set_damage, get = get_damage
+@export var knockback: bool = false: set = set_knockback, get = get_knockback
 
+@export_flags("HitEnvironment:2", "HitBreakable:4", "HitPlayer:8", "HitEnemy:64", "HitHazard:512") var layers : int = 0
+
+#region Setters and Getters
+func set_enabled(x: bool):
+	enabled = x
+	set_deferred("monitoring", x)
+
+func get_enabled():
+	return enabled
 
 func set_damage(value: int):
 	damage = value
 
-
 func get_damage() -> int:
 	return damage 
 
+func set_knockback(x: bool):
+	knockback = x
 
-#func _on_facing_changed(facing_right: bool) -> void: 
-#	if(facing_right): 
-#		position.x = 59
-#	else: 
-#		position.x = -59
+func get_knockback():
+	return knockback
+#endregion
+
+func update_mask():
+	collision_mask = layers
+
+func _ready():
+	set_enabled(enabled)
+	update_mask()
+
+func _on_area_entered(area: HurtBox) -> void:
+	if area.get_collision_layer_value(3) or area.get_collision_layer_value(4) or area.get_collision_layer_value(7):
+		area.take_damage(damage)
+	if knockback:
+		pass #TODO Knockback
